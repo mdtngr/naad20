e6553b374ece7ed0de6f392dfca2aa6531958357
mdtngr <mdtngr@gmail.com>
2019-09-02 16:23:02 +0530

    m

--- js/scripts5e1f.js
+++ js/scripts5e1f.js
@@ -135,23 +135,6 @@
 		---------------------*/
 		new WOW().init();
 	}
-	/* -------------------
-	Google map
-	---------------------*/
-	$("#map").gmap3({
-		marker:{
-		address:"44 W 66th St, New York, NY",
-		options:{ icon: "img/assets/marker.png"}},
-		map:{
-		options:{
-		styles: [ {
-		stylers: [ { "saturation":-90 }, { "lightness": 0 }, { "gamma": 0.0 }]},
-		],
-		zoom: 13,
-		scrollwheel:false,
-		draggable: true }
-		}
-	});
 
 	/* -------------------
 	Animated progress bars
