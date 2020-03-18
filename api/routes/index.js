9ce26e6d26ce2f11bca87d496b47a8ee3ff2ed30
ygyash <ygyashgoyal@gmail.com>
2020-03-18 12:41:03 +0530

    Made email feedback a async function

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const sendEmail = require('./../utils/sendEmail');
 const Registration = require('./../models/registration');
 
+
+
 router.get('/feedback', async (req, res) => {
     res.redirect('https://forms.gle/bCRnGm186hpeioUv5');
 })
@@ -15,14 +17,13 @@ router.get('/emailfeedback', async (req, res) => {
         return res.json("error");
     }
 
-    data.forEach((x) => {
-        //console.log(x);
+    for (let i = 0; i < data.length; i++) {
+        //console.log(data[i]);
         var link = "https://naad.dhwanibitmesra.live/naadwhitelogo.png";
         var link2 = "https://naad.dhwanibitmesra.live/api/feed/feedback"
-        sendEmail(x.email, "Thank you for participating in NAAD'20!", "<div style='text-align:center'><img src=" + link + "></div><br> Hi " + x.name + ",<br><br> Your participation has made our annual music celebration more festive than ever before. We were lucky to have you with us.<br><br> But most importantly, your feedback is valuable to us. We need it to make this fest better in the future. Kindly fill this feedback form <a href=" + link2 + ">here</a>. It will hardly take a minute or two.<br><br>Love you 3000! <br><br>Team NAAD");
-        //return res.json("success");
+        await sendEmail(data[i].email, "Thank you for participating in NAAD'20!", "<div style='text-align:center'><img src=" + link + "></div><br> Hi " + data[i].name + ",<br><br> Your participation has made our annual music celebration more festive than ever before. We were lucky to have you with us.<br><br> But most importantly, your feedback is valuable to us. We need it to make this fest better in the future. Kindly fill this feedback form <a href=" + link2 + ">here</a>. It will hardly take a minute or two.<br><br>Love you 3000! <br><br>Team NAAD");
+    }
 
-    });
     return res.json("success");
 
 });
--- api/utils/sendEmail.js
+++ api/utils/sendEmail.js
@@ -1,29 +1,34 @@
 const nodemailer = require("nodemailer");
 
-const sendEmail = (email,subject, msg) => {
-    let transport = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.emailUsername,
-            pass: process.env.emailPassword
-        },
-        secure: true,
-        pool: true
-    });
+const sendEmail = async (email, subject, msg) => {
+    return new Promise((resolve, reject) => {
+        let transport = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.emailUsername,
+                pass: process.env.emailPassword
+            },
+            secure: true,
+            pool: true
+        });
 
-    const message = {
-        from: process.env.emailUsername,
-        to: email,
-        subject: subject, // Subject line
-        html:`<p>${msg}</p>`
-    };
-    transport.sendMail(message, function (err, info) {
-        if (err) {
-            console.log(err);
-            return;
-        } else {
-            console.log("Email Sent");
-        }
+        const message = {
+            from: process.env.emailUsername,
+            to: email,
+            subject: subject, // Subject line
+            html: `<p>${msg}</p>`
+        };
+        //var resp = false;
+        transport.sendMail(message, function (err, info) {
+            if (err) {
+                console.log(err);
+                resolve(false);
+            } else {
+                console.log("Email Sent");
+                resolve(true);
+            }
+        });
+        //return resp;
     });
 }
 
